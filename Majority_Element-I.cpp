859c4aea1f223198002a48dd4a40e13973db6b16
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-04-10 19:36:23 +0530

    Added Majority Element-I

--- Majority_Element-I.cpp
+++ Majority_Element-I.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int freq=1;
-       int ans=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                freq++;
-            }
-            else{
-                freq=1;
-                ans=nums[i];
-            }
-        if(freq>n/2){
-            return ans;
+        int count=0;
+        int candidate;
+    for(int i=0;i<n;i++){
+        if(count==0){
+            candidate=nums[i];
+
         }
+         if(nums[i]==candidate){
+            count++;
         }
-       return ans; 
+        else{
+            count--;
+        }
+    }
+    return candidate;
     }
 };
