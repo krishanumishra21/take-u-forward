665980bb6c5113933154b0a9c7cb0d68d8eebc22
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-04-14 13:07:25 +0530

    Refactor Pascal's Triangle calculation method

--- Pascals_Triangle_I.cpp
+++ Pascals_Triangle_I.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int factorial(int n) {
-        if (n == 0) return 1;
-
-        int fact = 1;
-        for(int i = 1; i <= n; i++) {
-            fact = fact * i;
-        }
-        return fact;
-    }
-
     int pascalTriangleI(int r, int c) {
-        return factorial(r-1) / (factorial(c-1) * factorial(r-c));
+        int k=r-1;
+        int n=c-1;
+
+      long long result=1;
+        for(int i=0;i<n;i++){
+            result=result*(k-i);
+            result=result/(i+1);
+        }
+    return result;
     }
 };
