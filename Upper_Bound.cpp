d2e24125b5b2c53120ebb2eee21ece69d1a6b95f
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-04-19 03:05:21 +0530

    Added Upper Bound

---
+++ Upper_Bound.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+    int upperBound(vector<int> &nums, int x){
+                for(int i=0;i<nums.size();i++){
+            if(nums[i]>x){
+                return i;
+                break;
+            }
+        }
+        return nums.size();
+    }
+};
