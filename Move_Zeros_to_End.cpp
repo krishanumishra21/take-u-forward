3b5092d338adb9388935183829e7c8ac7db34d60
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-04-30 22:17:38 +0530

    Update Move_Zeros_to_End.cpp

--- Move_Zeros_to_End.cpp
+++ Move_Zeros_to_End.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
         int i=0;
-        for(int j=1;j<n;j++){
+        for(int j=0;j<n;j++){
             if(nums[j]!=0){
                 swap(nums[j],nums[i]);
                 i++;
