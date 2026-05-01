dcaf85a2eb021235bb2bc2eab29e0617cb1c4e63
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-05-01 15:45:57 +0530

    Added Intersection of two sorted arrays

--- Intersection_of_two_sorted_arrays.cpp
+++ Intersection_of_two_sorted_arrays.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<int> intersectionArray(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+                int a = nums1.size();
+        int b = nums2.size();
         int i = 0, j = 0;
-        int n = nums1.size(), m = nums2.size();
+        vector <int> inter;
+        while(i<a && j<b){
+            if(nums1[i]>nums2[j]){
+                j++;
 
-        vector<int> result;
-
-        while (i < n && j < m) {
-            if (nums1[i] < nums2[j]) {
+            }
+            else if(nums1[i]<nums2[j]){
                 i++;
             }
-            else if (nums1[i] > nums2[j]) {
-                j++;
-            }
-            else {
-                  if(result.empty() || result.back() != nums1[i]){
-                    result.push_back(nums1[i]);
-                }             
-
-                
+            else{
+                inter.push_back(nums1[i]);
                 i++;
                 j++;
             }
+           
         }
-
-        return result;
+         return inter;
     }
 };
